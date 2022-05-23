a57a0d0a8b7e90026be10c5bdd5471e7f4490865
roy897 <roushangupta85@gmail.com>
2022-05-23 16:37:37 +0530

    first commit

--- src/components/TaskApp.jsx
+++ src/components/TaskApp.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-
+import AddTask from "./AddTask/AddTask";
+import Task from "./Task/Task";
 import styles from "./taskApp.module.css";
+import TaskHeader from "./TaskHeader/TaskHeader"
 
 const TaskApp = () => {
   // NOTE: do not delete `data-cy` key value pair
   return (
     <div data-cy="task-app" className={styles.taskApp}>
       {/* Header */}
+      <TaskHeader/>
       {/* Add Task */}
-      {/* Tasks */}
+      <AddTask/>
+      {/* Tasks */}      
+      <Task/>
     </div>
   );
 };
